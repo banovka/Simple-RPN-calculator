b644104c540cf8c90c73ad169b8be4b2a6166fea
banovka <73527902+banovka@users.noreply.github.com>
2022-04-04 18:28:23 +0200

    Update layout.py

--- layout.py
+++ layout.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'rpn01.ui'
+## Form generated from reading UI file 'layout.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.2.4
 ##
